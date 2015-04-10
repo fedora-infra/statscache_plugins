b9ff935a237f6585c458123ab92df0d26b52a3ac
Ralph Bean <rbean@redhat.com>
2015-04-10 16:57:16 -0400

    Add another one.

--- statscache_plugins/volume/by_category.py
+++ statscache_plugins/volume/by_category.py
@@ -7,36 +7,36 @@ import sqlalchemy as sa
 
 def make_model(period):
     class Result(statscache.plugins.BaseModel):
-        __tablename__ = 'data_volume_by_topic_%i' % period
+        __tablename__ = 'data_volume_by_category_%i' % period
         timestamp = sa.Column(sa.DateTime, nullable=False, index=True)
         volume = sa.Column(sa.Integer, nullable=False)
-        topic = sa.Column(sa.UnicodeText, nullable=False, index=True)
+        category = sa.Column(sa.UnicodeText, nullable=False, index=True)
 
     return Result
 
 
 class Plugin(statscache.plugins.BasePlugin):
-    name = "volume, by topic"
-    summary = "the count of messages, organized by topic"
+    name = "volume, by category"
+    summary = "the count of messages, organized by category"
     description = """
     For any given time window, the number of messages that come across
-    the bus for each topic.
+    the bus for each category.
     """
     def handle(self, session, timestamp, messages):
-        topics = self.get_default_topics(session)
+        categories = self.get_default_categories(session)
         volumes = collections.defaultdict(int)
-        volumes.update(zip(topics, [0] * len(topics)))
+        volumes.update(zip(categories, [0] * len(categories)))
         for msg in messages:
-            volumes[msg['topic']] += 1
+            volumes[msg['topic'].split('.')[3]] += 1
 
-        for topic, volume in volumes.items():
+        for category, volume in volumes.items():
             result = self.model(
                 timestamp=timestamp,
                 volume=len(messages),
-                topic=topic)
+                category=category)
             session.add(result)
 
-    def get_default_topics(self, session):
-        # TODO -- get the existing topics out of the session
+    def get_default_categories(self, session):
+        # TODO -- get the existing categories out of the session
         # XXX - do we even want to do this?  Maybe not.
         return []
